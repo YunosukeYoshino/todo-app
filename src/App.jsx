2c55ecabd44f743e22aae0995d5398a89e9626fd
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2022-05-01 02:17:09 +0900

    🚩 toDOListの雛形を作成
    不要なファイルの削除を行なった

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export const App = () => {
+  return (
+    <div className="wrap">
+      <h1>ToDoList</h1>
+      <ul className="listWrap">
+        <li>test</li>
+      </ul>
+    </div>
+  );
+};
+
+export const todoList = () => {
+  return <></>;
+};
