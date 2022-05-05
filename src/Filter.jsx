315190614b77f1ef1e119311d995fcf870dfef7c
YunosukeYoshino <26004836+YunosukeYoshino@users.noreply.github.com>
2022-05-06 00:53:48 +0900

    🐛 Change to unique key

--- src/Filter.jsx
+++ src/Filter.jsx
@@ -1,14 +1,16 @@
 const filterOptions = [
-  { value: "all", label: "すべて" },
-  { value: "notStarted", label: "未着手" },
-  { value: "inProgress", label: "作業中" },
-  { value: "done", label: "完了" },
+  { id: 1, value: "all", label: "すべて" },
+  { id: 2, value: "notStarted", label: "未着手" },
+  { id: 3, value: "inProgress", label: "作業中" },
+  { id: 4, value: "done", label: "完了" },
 ];
 export const Filter = () => {
   return (
     <>
-      {filterOptions.map(({ value, label }) => (
-        <option value={value}>{label}</option>
+      {filterOptions.map(({ value, label, id }) => (
+        <option key={id} value={value}>
+          {label}
+        </option>
       ))}
     </>
   );
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,31 +1,33 @@
 import React from "react";
 import { Filter } from "./Filter";
 
-export const TodoList = ({ todo, deleteHandleClick, editClick }) => {
+export const TodoList = ({ todoItems, deleteHandleClick, editClick }) => {
   return (
     <>
-      <li
-        key={todo.id}
-        className="mt-6  bg-white p-6 rounded-md text-gray-500 shadow-lg"
-      >
-        <span className="mr-3">ID:{todo.id}</span>
-        <span className="mr-3">{todo.title}</span>
-        <select className="mr-3" name="status" value={todo.status}>
-          <Filter />
-        </select>
-        <button
-          className="bg-blue-400 py-2 px-4 rounded-md font-semibold"
-          onClick={() => editClick(todo)}
+      {todoItems.map((todo) => (
+        <li
+          key={todo.id}
+          className="mt-6  bg-white p-6 rounded-md text-gray-500 shadow-lg"
         >
-          編集
-        </button>
-        <button
-          className="bg-gray-300 py-2 px-4 mr-2 ml-2 rounded-md font-semibold"
-          onClick={() => deleteHandleClick(todo.id)}
-        >
-          削除
-        </button>
-      </li>
+          <span className="mr-3">ID:{todo.id}</span>
+          <span className="mr-3">{todo.title}</span>
+          <select className="mr-3" name="status" value={todo.status}>
+            <Filter />
+          </select>
+          <button
+            className="bg-blue-400 py-2 px-4 rounded-md font-semibold"
+            onClick={() => editClick(todo)}
+          >
+            編集
+          </button>
+          <button
+            className="bg-gray-300 py-2 px-4 mr-2 ml-2 rounded-md font-semibold"
+            onClick={() => deleteHandleClick(todo.id)}
+          >
+            削除
+          </button>
+        </li>
+      ))}
     </>
   );
 };
